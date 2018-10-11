8936d5dc3d3df4ea53735550e8f6b5c8c7616d0d
Fabio Waljaard <fabio.waljaard@gmail.com>
2018-10-11 17:17:26 +0200

    Implement editor saving to lilypond and midi

---
+++ DPA_Musicsheets/Editor/Saving/MidiSaver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DPA_Musicsheets.Converters;
+using DPA_Musicsheets.Models.Domain;
+
+namespace DPA_Musicsheets.Editor.Saving
+{
+    class MidiSaver : ICompositionSaver
+    {
+        public string Description => "MIDI";
+
+        public void Save(Composition composition, string filename)
+        {
+            var visitor = new ToMidiVisitor();
+            foreach (var token in composition.Tokens)
+            {
+                token.Accept(visitor);
+            }
+            var sequence = visitor.Sequence;
+            sequence.Save(filename);
+        }
+    }
+}
